8d80c06e7bd9e507c220cb200fb33efb64e66873
Arno <arno@cinagi.co.za>
2023-01-19 16:42:09 +0200

    Topic Module 6

--- wifi_app/views.py
+++ wifi_app/views.py
@@ -429,13 +429,17 @@ def exit_index(request):
         featured = Post.objects.filter(featured=True)[0:5]
         featured_other = Post.objects.filter(featured=True)[6:10]
         latest = Post.objects.order_by('-timestamp')[0:10]
-        topics = Topic.objects.all()
+        topics = Topic.objects.all().latest('pk')
+        the_id = Topic.objects.values_list('pk', flat=True).latest('pk')
+        print('the PK : ',the_id)
+        comments = Comment.objects.filter(topic = the_id).order_by('-pk')[0:4]
         context= {
             'object_list': featured,
             'featured_other': featured_other,
             'latest': latest,
             'categories':categories,
             'topics':topics,
+            'comments':comments,
            
       
         }
